000fa1d2138b6ba14d2450b7b937bbfc14809e82
Gohil Vishal <91748441+Gohilvd04@users.noreply.github.com>
2022-08-08 23:47:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const txtInput = document.querySelector(".inputs input");
+const checkBtn = document.querySelector(".inputs button");
+const infoTxt = document.querySelector(".info-text");
+let filterInput;
+
+checkBtn.addEventListener("click",()=>{
+    // spilitting user input character , reversing them ,
+    // and joining them in a single word 
+    let reverseInput = filterInput.split("").reverse().join("");
+    infoTxt.style.display = "block";
+    if(filterInput!=reverseInput){
+        return infoTxt.innerHTML = `No, <span>'${txtInput.value}'</span> isn't a Palindrome!`;
+    }
+    infoTxt.innerHTML = `Yes, <span>'${txtInput.value}'</span> is a Palindrome!`;
+
+})
+
+txtInput.addEventListener("keyup",()=>{
+    filterInput = txtInput.value.toLowerCase().replace(/[^A-Z0-9]/ig, "");
+    if(filterInput){
+        return checkBtn.classList.add("active");
+    }
+    infoTxt.style.display = "none";
+    checkBtn.classList.remove("active");
+
+})
